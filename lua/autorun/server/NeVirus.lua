44f41d0ffbbef8e59c27346261d9971c4993faa1
IvakaKiller <sepilovivan89@gmail.com>
2026-05-11 14:27:11 +0700

    что-то написал

--- lua/autorun/server/NeVirus.lua
+++ lua/autorun/server/NeVirus.lua
@@ -1,16 +1,57 @@
 math.randomseed(os.time())
 
+function safespawnfz(npcClass, originPos)
+	local maxAttem = 5
+	local searchRad = 200
+	for attem = 1, maxAttem do
+		local offsetX = math.random(-searchRadius, searchRadius)
+		local offsetY = math.random(-searchRadius, searchRadius)
+		local testPos = originPos + Vector(offsetX, offsetY, 0)
+		local traceData = {
+			start = testPos + Vector(0,0,10),
+			endpos = testPos - Vector(0,0,100),
+			filter = { },
+			mins = Vector(-16,-16,0),
+			maxs = Vector(16, 16, 72)
+		}
+		local trace = util.TraceHull(TraceData)
+		if !trace.Hit then
+			local npc = ents.Create(npcClass)
+			if IsValid(npc) then
+				npc:SetPos(trace.HitPos + Vector(0,0,1))
+				npc:Spawn()
+				return npc
+			end
+		end
+	end
+	return nil
+end
+
 local scarySounds = {
     "npc/zombie/zombie_pain1.wav",
     "npc/zombie/zombie_pain2.wav", 
     "npc/zombie/zombie_pain3.wav",
     "npc/fast_zombie/scare.wav",
-    "npc/headcrab/headcrab_alert1.wav",
-    "ambient/creatures/town_scary1.wav",
-    "ambient/creatures/town_scary2.wav",
-    "ambient/creatures/town_scary3.wav",
+    "ambient/creatures/town_muffled_cry1.wav",
+    "npc/strider/striderx_alert2.wav",
+    "npc/ministrider/hunter_laugh1.wav",
+    "ambient/sheep.wav",
     "ambient/levels/canals/headcrab_canister_open1.wav",
-    "ambient/alarms/citadel_alert_loop2.wav"
+    "ambient/alarms/citadel_alert_loop2.wav",
+	"ambient/creatures/town_zombie_call1.wav",
+	"ambient/voices/cough1.wav",
+	"ambient/voices/cough2.wav",
+	"ambient/voices/cough3.wav",
+	"ambient/voices/cough4.wav",
+	"common/bugreporter_succeeded.wav",
+	"npc/ministrider/hunter_angry1.wav",
+	"player/footsteps/grass1.wav",
+	"player/footsteps/grass4.wav",
+	"player/footsteps/concrete3.wav",
+	"player/footsteps/concrete2.wav",
+	"garrysmod/ui_click.wav",
+	"ambient/animal/cow.wav",
+	"ambient/animal/crow.wav"
 }
 
 hook.Add("PlayerInitialSpawn", "IgrokZashel", function(ply)
@@ -52,19 +93,18 @@ function ApplyRandomEffect(ply)
 		end},
 		{name = "ORDA ZOMBIES", func = function(p)
 			local pos = p:GetPos()
-			local zombies = {"npc_zombie", "npc_fastzombie", "npc_poisonzombie", "npc_headcrab", "npc_fastheadcrab"}
+			local zombies = {"npc_zombie", "npc_fastzombie", "npc_poisonzombie", "npc_headcrab"}
 			local zcount = math.random(4, 16)
 			for i = 1, zcount do
 				local zombieType = zombies[math.random(1, #zombies)]
-				local zombies = ents.Create(zombieType)
+				local zombie = ents.Create(zombieType)
 				if IsValid(zombie) then
-					local angle = (i / zcount) math.pi * 2
+					local angle = (i / zcount) * math.pi * 2
 					local dist = math.random(200,500)
 					local x = math.cos(angle) * math.random(150,300)
 					local y = math.sin(angle) * math.random(150,300)
 					zombie:SetPos(pos + Vector(x,y,0))
-					zombie:spawn()
-					
+					zombie:Spawn()
 				end
 			end
 		end}
